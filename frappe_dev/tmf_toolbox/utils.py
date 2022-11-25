20f3fe8807d381dde69435f6fe82ed810fa96b50
TMF42 <thomas.fojan@toolsense.io>
2022-11-25 08:42:40 +0100

    added guest_methods to whitelist

--- frappe_dev/tmf_toolbox/utils.py
+++ frappe_dev/tmf_toolbox/utils.py
@@ -5,4 +5,6 @@ def get_whitelisted_methods():
     whitelisted_functions = []
     for e in frappe.whitelisted:
         whitelisted_functions.append(f"{getattr(e, '__module__')}.{getattr(e, '__name__')}")
+    for e in frappe.guest_methods:
+        whitelisted_functions.append(f"{getattr(e, '__module__')}.{getattr(e, '__name__')}")
     return(whitelisted_functions)
